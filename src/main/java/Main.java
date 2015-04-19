aed1b75bae456ce0286a4100fbc37616805ed796
Anders Pitman <tapitman11@gmail.com>
2015-04-19 15:06:13 -0700

    Finish chapter 12

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,11 @@
+interface Expression {
+}
+
+class Bank {
+    Money reduce(Expression source, String to) {
+        return Money.dollar(10);
+    }
+}
 
 public class Main {
     public static void main(String[] argv) {
--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,4 +1,4 @@
-class Money {
+class Money implements Expression {
 
     protected int amount;
     protected String currency;
@@ -32,5 +32,9 @@ class Money {
 
     Money times(int multiplier) {
         return new Money(amount * multiplier, currency);
+    }
+
+    Expression plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
     }
 };
