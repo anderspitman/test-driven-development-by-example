24ba5b33d22d88d33c4c2f8a5c8b149cdb82a482
Anders Pitman <tapitman11@gmail.com>
2015-05-01 20:53:35 -0700

    Chapter 13

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,25 @@
 interface Expression {
+    public Money reduce(String to);
+}
+
+class Sum implements Expression {
+    Money augend;
+    Money addend;
+
+    Sum(Money augend, Money addend) {
+        this.augend = augend;
+        this.addend = addend;
+    }
+
+    public Money reduce(String to) {
+        int amount = augend.amount + addend.amount;
+        return new Money(amount, to);
+    }
 }
 
 class Bank {
     Money reduce(Expression source, String to) {
-        return Money.dollar(10);
+        return source.reduce(to);
     }
 }
 
--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -3,10 +3,6 @@ import static org.junit.Assert.*;
 
 public class TestMain {
 
-    //@Before
-    //public void setUp() {
-    //}
-
     @Test
     public void testMultiplication() {
         Money five = Money.dollar(5);
@@ -34,5 +30,29 @@ public class TestMain {
         Bank bank = new Bank();
         Money reduced = bank.reduce(sum, "USD");
         assertEquals(Money.dollar(10), reduced);
+    }
+
+    @Test
+    public void testPlusReturnsSum() {
+        Money five = Money.dollar(5);
+        Expression result = five.plus(five);
+        Sum sum = (Sum) result;
+        assertEquals(five, sum.augend);
+        assertEquals(five, sum.addend);
+    }
+
+    @Test
+    public void testReduceSum() {
+        Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
+        Bank bank = new Bank();
+        Money result = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(7), result);
+    }
+
+    @Test
+    public void testReduceMoney() {
+        Bank bank = new Bank();
+        Money result = bank.reduce(Money.dollar(1), "USD");
+        assertEquals(Money.dollar(1), result);
     }
 }
