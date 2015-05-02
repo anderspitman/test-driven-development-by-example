a434305e56992daea4fef37a162b85eb4332ff66
Anders Pitman <tapitman11@gmail.com>
2015-05-01 23:08:26 -0700

    Chapters 14 and 15

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,68 @@
+import java.util.Hashtable;
+
 interface Expression {
-    public Money reduce(String to);
+    public Money reduce(Bank bank, String to);
+    Expression plus(Expression addend);
 }
 
 class Sum implements Expression {
-    Money augend;
-    Money addend;
+    Expression augend;
+    Expression addend;
 
-    Sum(Money augend, Money addend) {
+    Sum(Expression augend, Expression addend) {
         this.augend = augend;
         this.addend = addend;
     }
 
-    public Money reduce(String to) {
-        int amount = augend.amount + addend.amount;
+    public Money reduce(Bank bank, String to) {
+        int amount = augend.reduce(bank, to).amount +
+            addend.reduce(bank, to).amount;
         return new Money(amount, to);
+    }
+
+    public Expression plus(Expression addend) {
+        return null;
     }
 }
 
 class Bank {
+    private Hashtable rates = new Hashtable();
+
     Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
+    }
+
+    void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), new Integer(rate));
+    }
+
+    int rate(String from, String to) {
+        if (from.equals(to)) return 1;
+        Integer rate = (Integer) rates.get(new Pair(from, to));
+        return rate.intValue();
     }
 }
 
 public class Main {
     public static void main(String[] argv) {
+    }
+}
+
+class Pair {
+    private String from;
+    private String to;
+
+    Pair(String from, String to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public boolean equals(Object object) {
+        Pair pair = (Pair) object;
+        return from.equals(pair.from) && to.equals(pair.to);
+    }
+
+    public int hashCode() {
+        return 0;
     }
 }
